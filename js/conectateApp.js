e64e58f3b313c0e31b6be21d2b9f0f563d081f9b
awakeelectronik <awakeelectronik@gmail.com>
2017-12-10 07:27:22 -0500

    Animaciones terminadas: Nubes y balanza. 3. Animaciones

--- js/conectateApp.js
+++ js/conectateApp.js
@@ -16,10 +16,40 @@ app.controller('conectateCtrl', function ($http, $q) {
 
     vm.auth = () =>  {
         vm.user = vm.info.prueba;
-        vm.user.mail = Object.keys(vm.info.prueba)[0] + "@uniandes.edu.co";
-        console.log(vm.user);
-    }
+        vm.user.mail = Object.keys(vm.info.prueba)[0] + "@uniandes.edu.co"; //pendiente
+        vm.calculateScales();
+    };
+
+    vm.calculateScales = function() {
+        vm.dom = {
+            programming: "scales" + Math.floor(vm.user.points.skill.programming/16),
+            abstraction: "scales" + Math.floor(vm.user.points.skill.abstraction/16),
+            paloLeft: "paloLeft" + Math.floor(vm.user.points.skill.abstraction/16),
+            paloRight: "paloRight" + Math.floor(vm.user.points.skill.programming/16)
+        };
+    };
     vm.getNumber = function(num) {
         return new Array(num);   
+    };
+    
+    vm.abstraction = function() {
+        if(vm.user.points.skill.abstraction>100){
+            vm.user.points.skill.abstraction=100;
+            vm.user.points.skill.programming=0;
+        } else if(vm.user.points.skill.abstraction<0){
+            vm.user.points.skill.abstraction=0;
+            vm.user.points.skill.programming=100;
+        } else vm.user.points.skill.programming=100-vm.user.points.skill.abstraction;
+        vm.calculateScales();
+    };
+    vm.programming = function() {
+        if(vm.user.points.skill.programming>100){
+            vm.user.points.skill.programming=100;
+            vm.user.points.skill.abstraction=0;
+        } else if(vm.user.points.skill.programming<0){
+            vm.user.points.skill.programming=0;
+            vm.user.points.skill.abstraction=100;
+        } else vm.user.points.skill.abstraction=100-vm.user.points.skill.programming;
+        vm.calculateScales();
     }
 });
